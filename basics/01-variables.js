5ed220de6e9d5f199c866673511f54e38faf0ee3
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-05-31 12:33:18 +0330

    Added 01-variables.js + Basic Variable Exercises

---
+++ basics/01-variables.js
@@ -0,0 +1,11 @@
+/**
+ * 01-variables.js
+ * 
+ * Basic Variable Exercises
+ * 
+ * 🎯 Tasks:
+ * 1. Create different types of variables
+ * 2. Practice variable reassignment
+ * 3. Work with constants
+ * 4. Try type conversion
+ */
