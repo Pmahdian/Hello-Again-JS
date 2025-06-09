4b1513be887d2e1aa629ac7335efded1992a9c62
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-06-09 15:27:37 +0330

    Comprehensive JavaScript Array Exercises

---
+++ basics/03-arrays.js
@@ -0,0 +1,12 @@
+/**
+ * 03-arrays.js
+ * 
+ * Comprehensive JavaScript Array Exercises
+ * 
+ * 📚 Topics Covered:
+ * 1. Array creation and access
+ * 2. Basic array methods (push, pop, shift, unshift)
+ * 3. Transformation methods (map, filter, reduce)
+ * 4. Search methods (find, some, every)
+ * 5. Other useful methods (slice, splice, concat, etc.)
+ */
