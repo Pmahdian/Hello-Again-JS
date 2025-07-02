b6d22bdc8d8f2416eb5b11e3e84b240c957b325f
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-03 02:30:53 +0330

    📚 Topics Covered

---
+++ basics/04-objects.js
@@ -0,0 +1,13 @@
+/**
+ * 04-objects.js
+ * 
+ * JavaScript Object Exercises
+ * 
+ * 📚 Topics Covered:
+ * 1. Object Creation and Properties
+ * 2. Methods and 'this' Context
+ * 3. Object Iteration
+ * 4. Object Destructuring
+ * 5. Object Spread Operator
+ * 6. Prototypes and Classes
+ */
