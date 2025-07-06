f88ef2f950cd5aa288360b09fd6945b92bd18cad
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-06 17:00:22 +0330

    JavaScript Conditional Statements Practice

---
+++ basics/05-conditionals.js
@@ -0,0 +1,13 @@
+/**
+ * 05-conditionals.js
+ * 
+ * JavaScript Conditional Statements Practice
+ * 
+ * 📚 Topics Covered:
+ * 1. if/else statements
+ * 2. Ternary operator
+ * 3. Switch statements
+ * 4. Logical operators
+ * 5. Nullish coalescing
+ * 6. Optional chaining
+ */
