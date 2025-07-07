f9200b093dc625a5b67802d7f3cd37186a6473ff
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-08 01:13:16 +0330

    JavaScript Loop Exercises

---
+++ basics/06-loops.js
@@ -0,0 +1,14 @@
+/**
+ * 06-loops.js
+ * 
+ * JavaScript Loop Exercises
+ * 
+ * 📚 Topics Covered:
+ * 1. for loops
+ * 2. while loops
+ * 3. do-while loops
+ * 4. for...of loops
+ * 5. for...in loops
+ * 6. Loop control (break, continue)
+ * 7. Array iteration methods
+ */
