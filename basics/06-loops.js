217809d30c0fb4c3daf02045daa7f4f242df80f5
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-08 21:09:31 +0330

    Find prime numbers up to N

--- basics/06-loops.js
+++ basics/06-loops.js
@@ -158,3 +158,23 @@ function multiplicationTable() {
 }
 console.log('Multiplication Table:');
 multiplicationTable();
+
+
+/**
+ * 2. Find prime numbers up to N
+ */
+function findPrimes(n) {
+  const primes = [];
+  for (let i = 2; i <= n; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) primes.push(i);
+  }
+  return primes;
+}
+console.log('Primes up to 20:', findPrimes(20));
