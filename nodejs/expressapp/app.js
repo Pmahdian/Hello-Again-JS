c7bdf8787ce4caff9b6df5db7d772310443c7cd7
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-08-22 23:11:09 +0330

    update

--- nodejs/expressapp/app.js
+++ nodejs/expressapp/app.js
@@ -6,8 +6,6 @@ app.get('./',(req,res)=>{
     req.send('hello express');
 });
 
-app.get('./api/users', (req,res)=>{
-    res.send
-} )
+
 const port  = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`listening on port ${port}`));
