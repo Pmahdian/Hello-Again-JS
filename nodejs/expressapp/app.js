4ce2fc4eba741ad5fa64dcc464a25f14b437d422
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-08-22 23:47:58 +0330

    update

--- nodejs/expressapp/app.js
+++ nodejs/expressapp/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 
 const app = express();
-app.use(express.json());
+
+app.use(express.json());  //برای خواندن اطلاعات بادی
+
 app.get('./',(req,res)=>{
     req.send('hello express');
 });
