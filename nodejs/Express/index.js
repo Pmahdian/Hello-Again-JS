0d2568255d23fcf1d6a3840c2cc0dbf818ba44c2
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-09-30 00:55:49 +0330

    copplete put api

--- nodejs/Express/index.js
+++ nodejs/Express/index.js
@@ -42,6 +42,11 @@ app.put('/api/courses/:id', (req,res)=>{
     const course = courses.find(c=> c.id === parseInt(req.params.id));
     if(!course) return res.status(404).send('the course with given id not found');
 
+    if (!req.body.name || req.body.name.length < 3) 
+        return res.status(400).send('name is empty or less than 3 char')
+    course.name = req.body.name
+    res.send(course)
+
 })
 
 
