ec383be3e090da85fe363ca09399981649499ee8
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-09-30 21:34:46 +0330

    update

--- nodejs/Express/index.js
+++ nodejs/Express/index.js
@@ -43,9 +43,9 @@ app.put('/api/courses/:id', (req,res)=>{
     if(!course) return res.status(404).send('the course with given id not found');
 
     if (!req.body.name || req.body.name.length < 3) 
-        return res.status(400).send('name is empty or less than 3 char')
-    course.name = req.body.name
-    res.send(course)
+        return res.status(400).send('name is empty or less than 3 char');
+    course.name = req.body.name;
+    res.send(course);
 
 })
 
