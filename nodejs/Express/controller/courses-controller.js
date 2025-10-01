0547489589058a1e51f612f86e9d7ec62515be5b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-01 15:00:46 +0330

    set updateCourse func for put api

--- nodejs/Express/controller/courses-controller.js
+++ nodejs/Express/controller/courses-controller.js
@@ -30,7 +30,20 @@ const insertCourse = (req,res)=>{
     res.send(course)
 }
 
+
+
+const updateCourse = (req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('the course with given id not found');
+
+    if (!req.body.name || req.body.name.length < 3) 
+        return res.status(400).send('name is empty or less than 3 char');
+    course.name = req.body.name;
+    res.send(course);
+
+}
+
 module.exports = {
-    getCourse, insertCourse, getCourses
+    getCourse, insertCourse, getCourses, updateCourse
 
 }
--- nodejs/Express/routes/courses-route.js
+++ nodejs/Express/routes/courses-route.js
@@ -11,16 +11,7 @@ router.get('/', coursesController.getCourses);
 
 router.post('/', coursesController.insertCourse);
 
-router.put('/:id', (req,res)=>{
-    const course = courses.find(c=> c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('the course with given id not found');
-
-    if (!req.body.name || req.body.name.length < 3) 
-        return res.status(400).send('name is empty or less than 3 char');
-    course.name = req.body.name;
-    res.send(course);
-
-})
+router.put('/:id', coursesController.updateCourse);
 
 
 module.exports = router
