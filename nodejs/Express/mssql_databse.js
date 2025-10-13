b4357308442a4a4c05049ac71d6e0db53d797374
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-13 23:14:13 +0330

    update

--- nodejs/Express/mssql_databse.js
+++ nodejs/Express/mssql_databse.js
@@ -1,1 +1,14 @@
 const sql = require('mssql');
+require('dotenv').config();
+
+const config = {
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    server : process.env.DB_HOST,
+    database : process.env.DB_DATABASE,
+    options : {
+        encrypt : false,
+        TrustServerCertificate : true
+    }
+
+}
