6f1958bd56530295d0700204e296fe9f535bc396
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-16 21:57:37 +0330

    def register and login into users-controller

---
+++ nodejs/Express/controller/users-controller.js
@@ -0,0 +1,13 @@
+userModel = require('../models/users-model');
+
+const register = async (req, res, next)=> {
+
+};
+const login = async (req, res, next)=> {
+
+
+};
+
+module.exports = {
+    register,login
+};
