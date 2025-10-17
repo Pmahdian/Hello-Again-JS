0bcf2f042c883c7959c47525c10d3b05a1c058b9
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-17 23:50:25 +0330

    password validation

--- nodejs/Express/controller/users-controller.js
+++ nodejs/Express/controller/users-controller.js
@@ -7,7 +7,7 @@ const register = async (req, res, next)=> {
     const schema= {
         name : Joi.string().min(3).max(50).required(),
         email : Joi.string().email().required(),
-        password : Joi.string()
+        password : Joi.string().min(5).max(50).required() 
     }
     res.send('ok');
 
