6e11c4e17b9626331ab0f3ec9595997e8f604084
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-18 03:24:55 +0330

    write inserUser model for inserting new user to database table (users)

--- nodejs/Express/models/users-model.js
+++ nodejs/Express/models/users-model.js
@@ -1,6 +1,11 @@
 const pool = require('../utilities/mysql_database');
 
 class UserModel {
+    static insertuser = async(name, email, password) =>{
+        const [result] = pool.query(`insert into users (id, name, email, password)values (uuid(), ?, ?, ?)`, [name, email, password])
+        return result;  
 
+
+} 
 }
 module.exports = UserModel;
