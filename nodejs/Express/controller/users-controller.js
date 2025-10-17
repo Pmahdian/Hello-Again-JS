dbbcc2538d69c8efc01d1033fd2fe01211c2980d
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-17 23:13:45 +0330

    sending a massage to user how send us a req

--- nodejs/Express/controller/users-controller.js
+++ nodejs/Express/controller/users-controller.js
@@ -2,6 +2,7 @@ userModel = require('../models/users-model');
 
 const register = async (req, res, next)=> {
     console.log(req.body);
+    res.send('ok');
 
 };
 const login = async (req, res, next)=> {
