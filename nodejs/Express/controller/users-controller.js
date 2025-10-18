67403b0d1706f8566f187f543e701a47d1373a54
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-18 20:39:31 +0330

    write schema in login mid

--- nodejs/Express/controller/users-controller.js
+++ nodejs/Express/controller/users-controller.js
@@ -35,6 +35,16 @@ const register = async (req, res, next)=> {
 
 };
 const login = async (req, res, next)=> {
+    const schema= {
+        email : Joi.string().email().required(),
+        password : Joi.string().min(5).max(50).required() 
+    }
+    const validateResult =  Joi.object(schema).validate(req.body );
+    console.log(validateResult);
+    if (validateResult.error)
+        return res.send(validateResult.error.details[0].message)
+    
+
 
 
 };
@@ -42,3 +52,8 @@ const login = async (req, res, next)=> {
 module.exports = {
     register,login
 };
+
+
+
+
+
