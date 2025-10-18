e55122d03c21eb5cd4e242f7db63650a5c6f6370
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-19 00:50:48 +0330

    token validation with try and catach

--- nodejs/Express/middelwares/auth.js
+++ nodejs/Express/middelwares/auth.js
@@ -8,7 +8,14 @@ function auth(req, res, next){
     const token = req.header('Authorization')
     if (!token) return res.status(401).send('Access denied')
 
-    jwt.verify(token, process.env.SECRET_KEY)
+
+    try{  
+    const decode = jwt.verify(token, process.env.SECRET_KEY)
+    req.userData = decode
+    }
+    catch(er){
+        res.satatus(400).send('token is invalid')
+    }
 
 
 }
