b50086a00cac97a2de61df9ce7ed52ec8019939e
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-19 00:41:11 +0330

    import jwt

--- nodejs/Express/middelwares/auth.js
+++ nodejs/Express/middelwares/auth.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken');
+
 function auth(req, res, next){
     const token = req.header('Authorization')
+    if (!token) return res.status(401).send('Access denied')
+
+
 }
