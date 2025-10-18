2ba942d6aa2c7672df2f64a6485af68925e8bc15
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-19 00:53:18 +0330

    export auth func

--- nodejs/Express/middelwares/auth.js
+++ nodejs/Express/middelwares/auth.js
@@ -4,7 +4,7 @@ const path = require('path');
 // مسیر صحیح به فایل .env (یک پوشه بالاتر)
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
-function auth(req, res, next){
+module.exports = function auth(req, res, next){
     const token = req.header('Authorization')
     if (!token) return res.status(401).send('Access denied')
 
