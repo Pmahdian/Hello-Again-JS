864c077264f2bb7fc6582f0e1a6fa93818e1fe7f
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-19 20:26:42 +0330

    import auth middelware to courses-route

--- nodejs/Express/routes/courses-route.js
+++ nodejs/Express/routes/courses-route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const coursesController = require("../controller/courses-controller");
+const auth = require('../middelwares/auth');
 
 
 
